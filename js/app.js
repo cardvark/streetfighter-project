80f273c6f8347f5242d120ae3da8110b5b3ee581
cardvark <endaction@gmail.com>
2015-06-07 17:55:03 -0700

    Minor formatting adjustment to js

--- js/app.js
+++ js/app.js
@@ -1,47 +1,49 @@
 $(document).ready(function(){
-	$('.ryu').mouseenter(function(){
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		playHadouken();
-		$('.hadouken').finish().show().animate(
-			{'left': '1020px'},
-			500,
-			function(){
-				$(this).hide();
-				$(this).css('left','520px');
-			}
-		);
-	})
-	.mouseup(function(){
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	})
+	$('.ryu')
+		.mouseenter(function(){
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+		})
+		.mouseleave(function(){
+			$('.ryu-ready').hide();
+			$('.ryu-still').show();
+		})
+		.mousedown(function(){
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').show();
+			playHadouken();
+			$('.hadouken').finish().show().animate(
+				{'left': '1020px'},
+				500,
+				function(){
+					$(this).hide();
+					$(this).css('left','520px');
+				}
+			);
+		})
+		.mouseup(function(){
+			$('.ryu-throwing').hide();
+			$('.ryu-ready').show();
+		})
 	;
 
-	$(document).keydown(function(e){
-		var keycode = (e.keyCode ? e.keyCode : e.which);
-		if (keycode == '88') {
-			$('.ryu-ready').hide();
-			$('.ryu-still').hide();
-			$('.ryu-throwing').hide();
-			$('.ryu-cool').show();
-		}
-	})
-	.keyup(function(e){
-		var keycode = (e.keyCode ? e.keyCode : e.which);
-		if (keycode == '88') {
-			$('.ryu-cool').hide();
-			$('.ryu-still').show();
-		}
-	})
+	$(document)
+		.keydown(function(e){
+			var keycode = (e.keyCode ? e.keyCode : e.which);
+			if (keycode == '88') {
+				$('.ryu-ready').hide();
+				$('.ryu-still').hide();
+				$('.ryu-throwing').hide();
+				$('.ryu-cool').show();
+			}
+		})
+		.keyup(function(e){
+			var keycode = (e.keyCode ? e.keyCode : e.which);
+			if (keycode == '88') {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			}
+		})
 	;
 
 });
